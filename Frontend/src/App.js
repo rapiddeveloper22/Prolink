db2ece54bcf3b9c7a7bdb159f61dfafa84ba1cc9
rapiddeveloper22 <mmaswin22@gmail.com>
2023-10-04 21:46:35 +0530

    Routing done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Profile from './pages/Profile';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Sign_up from './pages/Sign_up;'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className='container'>
+                <ul>
+                    <li>
+                        <Link to='/Login'>Login</Link>
+                    </li>
+                    <li>
+                        <Link to='/Sign_up'>Sign up</Link>
+                    </li>
+                </ul>
+
+                <Routes>
+                    <Route exact path="/Login" element={<Login />} />
+                    <Route exact path="/Sign_up" element={<Sign_up />} />
+                </Routes>
+
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ Frontend/src/pages/Profile.js
@@ -0,0 +1,14 @@
+function Profile() {
+    return (
+        <div className="container">
+            <div className="user-details">
+                <p id="username">User name : <span>Aswin M M</span></p>
+                <p id="useremail">Email : <span>mmaswin22@gmail.com</span></p>
+                <p id="phonenumber">Phone number : <span>9489526610</span></p>
+                <img src="../../dummy_data/batman.jpg" alt="Profile image" />
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
